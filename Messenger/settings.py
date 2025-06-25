055d2c3245c1193b76ee36741e6aa6d32cdc4a15
Dima <you@example.com>
2025-06-25 19:02:34 +0200

    installed whitenoise

--- Messenger/settings.py
+++ Messenger/settings.py
@@ -55,6 +55,7 @@ CHANNEL_LAYERS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
